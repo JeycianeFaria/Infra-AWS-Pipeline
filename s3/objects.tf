d8cd1ce2d87f7d708e8a13cead2336d1436270b9
Jeyciane Faria <jeycianedeveloper@gmail.com>
2022-05-05 18:55:50 -0300

    Provisionar objetos no bucket s3

---
+++ s3/objects.tf
@@ -0,0 +1,33 @@
+resource "aws_s3_object" "raw-data-empresa" { 
+  bucket = aws_s3_bucket.jaf-pipeline-dado.id
+  key    = "raw-datas/empresas/"
+}
+
+resource "aws_s3_object" "raw-data-socios" { 
+  bucket = aws_s3_bucket.jaf-pipeline-dado.id
+  key    = "raw-datas/socios/"
+}
+
+resource "aws_s3_object" "processed-data" {
+  bucket = aws_s3_bucket.jaf-pipeline-dado.id
+  key    = "processed-data/"
+}
+
+resource "aws_s3_object" "scripts" {
+  count = length(var.scripts_glue)  
+  bucket = aws_s3_bucket.jaf-pipeline-dado.id
+  key    = "jobs/${var.scripts_glue[count.index].script_name}"
+  source = "scripts/${var.scripts_glue[count.index].script_name}"
+}
+
+resource "aws_s3_object" "deeque_jar" {
+  bucket = aws_s3_bucket.jaf-pipeline-dado.id
+  key = "libs/deequ-1.2.2-spark-2.4.jar"
+  source =  "libs/deequ-1.2.2-spark-2.4.jar"
+}
+
+resource "aws_s3_object" "pydeequ_zip" {
+  bucket = aws_s3_bucket.jaf-pipeline-dado.id
+  key = "libs/pydeequ.zip"
+  source =  "libs/pydeequ.zip"
+}
